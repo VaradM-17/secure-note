774b77631b61ccc1d65513bde65519bebac251fd
Varad Mule <varadmule17@gmail.com>
2026-03-12 19:13:51 +0530

    defined userdetails and userdetailsImpl

---
+++ notes/src/main/java/com/varad/secure/notes/config/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.varad.secure.notes.config.services;
+
+import com.varad.secure.notes.entity.User;
+import com.varad.secure.notes.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+
+
+}
+
